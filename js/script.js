c871dd2e4c266a510962ce307afc35f480df66ef
AbiRubrico <2165751@slu.edu.ph>
2019-04-09 17:15:38 +0800

    AbiRubrico edited script.js

--- js/script.js
+++ js/script.js
@@ -3,5 +3,11 @@ var tabPanels=document.querySelectorAll("div.buttonsContainer button");
 
 
 function showPanel(panelIndex, colorCode){
-	
+	tabButtons.forEach(function(node){
+		node.style.backgroundColor="";
+		node.style.color="";
+	});
+	tabButtons[panelIndex].style.backgroundColor=colorCode;
+	tabButtons[panelIndex].style.color="white";
+
 }
