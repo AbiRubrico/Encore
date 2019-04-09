678e2fff2da88013f27d1172b28b7b347c9007be
AbiRubrico <2165751@slu.edu.ph>
2019-04-09 17:07:31 +0800

    AbiRubrico edited script.js

--- js/script.js
+++ js/script.js
@@ -1,2 +1,7 @@
 var tabButtons=document.querySelectorAll("div.buttonsContainer button");
 var tabPanels=document.querySelectorAll("div.buttonsContainer button");
+
+
+function showPanel(panelIndex, colorCode){
+	
+}
